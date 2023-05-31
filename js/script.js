0a9364da623e6f2275742308201ac9384a74c1b4
Neordan <83787849+Neordan@users.noreply.github.com>
2023-06-01 01:09:44 +0200

    Création de la fonction bubbleMaker

--- js/script.js
+++ js/script.js
@@ -1,28 +1,34 @@
-//Création de la constante bubble
-const bubble = document.createElement('span');
+//Création de la fonction bubbleMaker
 
-//Ajout d'une classe au span
-bubble.classList.add('bubble');
+const bubbleMaker = () => {
 
-//Injection de bubble dans le body
-document.body.appendChild(bubble);
+    //Création de la constante bubble
+    const bubble = document.createElement('span');
 
-//Création d'une taille random pour les bubbles + taille min 100px
-const size = Math.random() * 200 + 100 + "px";
+    //Ajout d'une classe au span
+    bubble.classList.add('bubble');
 
- //Ajout de la hauteur et la longueur dans sa classe
+    //Injection de bubble dans le body
+    document.body.appendChild(bubble);
 
- bubble.style.height = size;
- bubble.style.width = size;
+    //Création d'une taille random pour les bubbles + taille min 100px
+    const size = Math.random() * 200 + 100 + "px";
 
- //Création de l'emplacement random par rapport au top de la page pour les bubble & toujours à 60% pour qu'elles se créent vers le bas
+    //Ajout de la hauteur et la longueur dans sa classe
 
- bubble.style.top = Math.random() * 100 + 60 + "%";
+    bubble.style.height = size;
+    bubble.style.width = size;
 
- //Création de l'emplacement random par rapport au coté gauche de la page pour les bubble pour un effet de flottement
- 
- bubble.style.left = Math.random() * 100 + "%";
+    //Création de l'emplacement random par rapport au top de la page pour les bubble & toujours à 60% pour qu'elles se créent vers le bas
 
- //Utilisation de setProperty pour changer la valeur de left en mode random
+    bubble.style.top = Math.random() * 100 + 60 + "%";
 
- bubble.style.setProperty('--left', Math.random() * 100 + "%")
+    //Création de l'emplacement random par rapport au coté gauche de la page pour les bubble pour un effet de flottement
+
+    bubble.style.left = Math.random() * 100 + "%";
+
+    //Utilisation de setProperty pour changer la valeur de left en mode random
+
+    bubble.style.setProperty('--left', Math.random() * 100 + "%")
+
+}

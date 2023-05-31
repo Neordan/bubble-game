273283045f5829ddaafa67cc402810c55003fb3d
Neordan <83787849+Neordan@users.noreply.github.com>
2023-06-01 00:49:46 +0200

    Création de l'emplacement random par rapport au top de la page pour les bubble & toujours à 60% pour qu'elles se créent vers le bas

--- js/script.js
+++ js/script.js
@@ -7,10 +7,14 @@ bubble.classList.add('bubble');
 //Injection de bubble dans le body
 document.body.appendChild(bubble);
 
-//Création d'une taille random pour les bubble + taille min 100px
+//Création d'une taille random pour les bubbles + taille min 100px
 const size = Math.random() * 200 + 100 + "px";
 
  //Ajout de la hauteur et la longueur dans sa classe
 
  bubble.style.height = size;
  bubble.style.width = size;
+
+ //Création de l'emplacement random par rapport au top de la page pour les bubble & toujours à 60% pour qu'elles se créent vers le bas
+
+ bubble.style.top = Math.random() * 100 + 60 + "%";

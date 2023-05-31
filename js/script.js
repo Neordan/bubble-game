942ca9037a674db3ab820071df520cf09949bf08
Neordan <83787849+Neordan@users.noreply.github.com>
2023-06-01 00:30:49 +0200

    Création d'un nombre random pour la taille de la bubble

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
+//Création de la constante bubble
 const bubble = document.createElement('span');
 
 //Ajout d'une classe au span
 bubble.classList.add('bubble');
 
 //Injection de bubble dans le body
-
 document.body.appendChild(bubble);
+
+//Création d'une taille random pour les bubble + taille min 100px
+const size = Math.random() * 200 + 100 + "px";
+
+ //Ajout de la hauteur et la longueur dans sa classe

5f5f9d69d04f637fb9a83db632fd689ef40a9c15
Neordan <83787849+Neordan@users.noreply.github.com>
2023-06-01 00:07:08 +0200

    Ajout d'une classe aux spans

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const bubble = document.createElement('span');
 
+//Ajout d'une classe au span
+bubble.classList.add('bubble');
+
 //Injection de bubble dans le body
 
 document.body.appendChild(bubble);

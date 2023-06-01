03c3802f06e7f02a072cc6e573ef3fa783439648
Neordan <83787849+Neordan@users.noreply.github.com>
2023-06-01 20:35:36 +0200

    Ajouter un écouteur d'événement pour le chargement de la fenêtre

--- js/script.js
+++ js/script.js
@@ -1,56 +1,58 @@
-// Récupération du h3 
-const compteur = document.querySelector('h3');
+window.addEventListener('load', () => {
+    // Code à exécuter une fois que la page est entièrement chargée
 
-// Création de la fonction bubbleMaker
+    // Récupération du h3
+    const counterDisplay = document.querySelector('h3');
 
-const bubbleMaker = () => {
+    // Création d'une variable counter
+    let counter = 0;
 
-    // Création de la constante bubble
-    const bubble = document.createElement('span');
+    // Création de la fonction bubbleMaker
+    const bubbleMaker = () => {
+        // Création de la constante bubble
+        const bubble = document.createElement('span');
 
-    // Ajout d'une classe au span
-    bubble.classList.add('bubble');
+        // Ajout d'une classe au span
+        bubble.classList.add('bubble');
 
-    // Injection de bubble dans le body
-    document.body.appendChild(bubble);
+        // Injection de bubble dans le body
+        document.body.appendChild(bubble);
 
-    // Création d'une taille random pour les bubbles + taille min 100px
-    const size = Math.random() * 200 + 100 + "px";
+        // Création d'une taille random pour les bubbles + taille min 100px
+        const size = Math.random() * 200 + 100 + "px";
 
-    // Ajout de la hauteur et la longueur dans sa classe
+        // Ajout de la hauteur et de la largeur dans sa classe
+        bubble.style.height = size;
+        bubble.style.width = size;
 
-    bubble.style.height = size;
-    bubble.style.width = size;
+        // Création de l'emplacement random par rapport au top de la page pour les bubble & toujours à 60% pour qu'elles se créent vers le bas
+        bubble.style.top = Math.random() * 100 + 60 + "%";
 
-    // Création de l'emplacement random par rapport au top de la page pour les bubble & toujours à 60% pour qu'elles se créent vers le bas
+        // Création de l'emplacement random par rapport au côté gauche de la page pour les bubble pour un effet de flottement
+        bubble.style.left = Math.random() * 100 + "%";
 
-    bubble.style.top = Math.random() * 100 + 60 + "%";
+        // Création d'une valeur random négative ou positive pour que les bubbles se croisent
+        const plusMinus = Math.random() > 0.5 ? 1 : -1;
 
-    // Création de l'emplacement random par rapport au coté gauche de la page pour les bubble pour un effet de flottement
+        // Utilisation de setProperty pour changer la valeur de left en mode random en utilisant aussi plusMinus
+        bubble.style.setProperty('--left', Math.random() * 100 * plusMinus + "%");
 
-    bubble.style.left = Math.random() * 100 + "%";
-    
-    // Création d'une valeur random négatif ou positif pour que les bubbles se croisent  
-    const plusMinus = Math.random() > 0.5 ?1 : -1;
+        // Ajout d'un événement au clic pour éclater les bubbles avec incrémentation du compteur
+        bubble.addEventListener("click", () => {
+            counter++;
+            counterDisplay.textContent = counter;
+            bubble.remove();
+        });
 
-    // Utilisation de setProperty pour changer la valeur de left en mode random en utilisant aussi plusMinus
+        // Autodestruction des bubbles au bout de 8s
+        setTimeout(() => {
+            // Supprimer les bubbles
+            bubble.remove();
+        }, 8000);
+    };
 
-    bubble.style.setProperty('--left', Math.random() * 100 * plusMinus + "%");
 
-    // Ajout d'un evenement au clique pour éclater les bubbles
+    // Toutes les 1 seconde, création d'une bubble suite à l'appel de la fonction bubbleMaker
+    setInterval(bubbleMaker, 1000);
 
-    bubble.addEventListener("click", () => {
-        bubble.remove();
-    })
-    
-    // Autodestruction des bubbles au bout de 8s
-    
-    setTimeout(() => {
-        // Supprimer les bubbles
-        bubble.remove();
-    }, 8000);
-};
-
-// Toute les 0.3 secondes, création d'une bubble suite à la l'appel de la fonction bubbleMaker
-
-setInterval(bubbleMaker, 1000);
+});

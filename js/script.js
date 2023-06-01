ac427e790bbb13cfdde4764f964380097e8e4e1a
Neordan <83787849+Neordan@users.noreply.github.com>
2023-06-01 12:41:05 +0200

    Ajout d'un evenement au clique pour éclater les bubbles + Autodestruction des bubbles au bout de 8s

--- js/script.js
+++ js/script.js
@@ -1,40 +1,53 @@
-//Création de la fonction bubbleMaker
+// Création de la fonction bubbleMaker
 
 const bubbleMaker = () => {
 
-    //Création de la constante bubble
+    // Création de la constante bubble
     const bubble = document.createElement('span');
 
-    //Ajout d'une classe au span
+    // Ajout d'une classe au span
     bubble.classList.add('bubble');
 
-    //Injection de bubble dans le body
+    // Injection de bubble dans le body
     document.body.appendChild(bubble);
 
-    //Création d'une taille random pour les bubbles + taille min 100px
+    // Création d'une taille random pour les bubbles + taille min 100px
     const size = Math.random() * 200 + 100 + "px";
 
-    //Ajout de la hauteur et la longueur dans sa classe
+    // Ajout de la hauteur et la longueur dans sa classe
 
     bubble.style.height = size;
     bubble.style.width = size;
 
-    //Création de l'emplacement random par rapport au top de la page pour les bubble & toujours à 60% pour qu'elles se créent vers le bas
+    // Création de l'emplacement random par rapport au top de la page pour les bubble & toujours à 60% pour qu'elles se créent vers le bas
 
     bubble.style.top = Math.random() * 100 + 60 + "%";
 
-    //Création de l'emplacement random par rapport au coté gauche de la page pour les bubble pour un effet de flottement
+    // Création de l'emplacement random par rapport au coté gauche de la page pour les bubble pour un effet de flottement
 
     bubble.style.left = Math.random() * 100 + "%";
     
-    //Création d'une valeur random négatif ou positif pour que les bubbles se croisent  
+    // Création d'une valeur random négatif ou positif pour que les bubbles se croisent  
     const plusMinus = Math.random() > 0.5 ?1 : -1;
 
-    //Utilisation de setProperty pour changer la valeur de left en mode random en utilisant aussi plusMinus
+    // Utilisation de setProperty pour changer la valeur de left en mode random en utilisant aussi plusMinus
 
-    bubble.style.setProperty('--left', Math.random() * 100 * plusMinus + "%")
+    bubble.style.setProperty('--left', Math.random() * 100 * plusMinus + "%");
+
+    // Ajout d'un evenement au clique pour éclater les bubbles
+
+    bubble.addEventListener("click", () => {
+        bubble.remove();
+    })
+    
+    // Autodestruction des bubbles au bout de 8s
+    
+    setTimeout(() => {
+        // Supprimer les bubbles
+        bubble.remove();
+    }, 8000);
 };
 
-//toute les 0.3 secondes, création d'une bubble suite à la l'appel de la fonction bubbleMaker
+// Toute les 0.3 secondes, création d'une bubble suite à la l'appel de la fonction bubbleMaker
 
-setInterval(bubbleMaker, 300);
+setInterval(bubbleMaker, 1000);
